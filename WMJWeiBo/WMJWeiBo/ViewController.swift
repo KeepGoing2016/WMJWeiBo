351cd6acbbc5c701426812aa6ca1defd1ac4aa00
KeepGoing2016 <huashuiyijia01@163.com>
2016-06-27 18:03:56 +0800

    添加微博项目

---
+++ WMJWeiBo/WMJWeiBo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WMJWeiBo
+//
+//  Created by lumf on 16/6/27.
+//  Copyright © 2016年 WMJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
