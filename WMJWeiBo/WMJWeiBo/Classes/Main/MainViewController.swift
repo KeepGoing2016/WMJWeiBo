170d2ffaa597fce7dbf7abe031fca0afc6a26497
KeepGoing2016 <huashuiyijia01@163.com>
2016-06-28 15:03:45 +0800

    通过字符串创建类（命名空间）

--- WMJWeiBo/WMJWeiBo/Classes/Main/MainViewController.swift
+++ WMJWeiBo/WMJWeiBo/Classes/Main/MainViewController.swift
@@ -18,17 +18,38 @@ class MainViewController: UITabBarController {
 
     func addChildviewControllers()->Void{
         
-        addChildViewController(HomeTableViewController(), title: "首页", imageName:"tabbar_home" )
+        //通过传入类创建
+//        addChildViewController(HomeTableViewController(), title: "首页", imageName:"tabbar_home" )
+//        
+//        addChildViewController(MessageTableViewController(), title: "消息", imageName: "tabbar_message_center")
+//        
+//        addChildViewController(DiscoverTableViewController(), title: "广场", imageName: "tabbar_discover")
+//        
+//        addChildViewController(MessageTableViewController(), title: "我", imageName: "tabbar_profile")
         
-        addChildViewController(MessageTableViewController(), title: "消息", imageName: "tabbar_message_center")
-        
-        addChildViewController(DiscoverTableViewController(), title: "广场", imageName: "tabbar_discover")
-        
-        addChildViewController(MessageTableViewController(), title: "我", imageName: "tabbar_profile")
+        //通过字符串创建
+        addChildViewController("HomeTableViewController", title: "首页", imageName:"tabbar_home" )
+
+        addChildViewController("MessageTableViewController", title: "消息", imageName: "tabbar_message_center")
+
+        addChildViewController("DiscoverTableViewController", title: "广场", imageName: "tabbar_discover")
+
+        addChildViewController("MessageTableViewController", title: "我", imageName: "tabbar_profile")
         
     }
 
-    func addChildViewController(childController: UITableViewController,title:String,imageName:String) {
+//    func addChildViewController(childController: UITableViewController,title:String,imageName:String) {
+    func addChildViewController(childControllerString: String,title:String,imageName:String) {
+        
+        //获取命名空间，一个项目的命名空间就是该项目的名称
+        let nameSpace = NSBundle.mainBundle().infoDictionary!["CFBundleExecutable"] as! String
+        let classNameStr = nameSpace + "." + childControllerString
+        let cls:AnyClass =  NSClassFromString(classNameStr)!
+        
+        let clsClass = cls as!UITableViewController.Type
+        
+        let childController = clsClass.init()
+        
         childController.title = title
         childController.tabBarItem.image = UIImage(named: imageName)
         childController.tabBarItem.image = UIImage(named: imageName+"_highlighted")
