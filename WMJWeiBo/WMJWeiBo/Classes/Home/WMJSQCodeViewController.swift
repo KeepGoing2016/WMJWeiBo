741bc785a425b3b63dd7ec5501db15eafd3cf8ed
KeepGoing2016 <huashuiyijia01@163.com>
2016-07-04 14:52:33 +0800

    1、完成二维码扫描描边功能 2、设置二维码扫描区域 3、完成从相册读取二维码功能 4、完成二维码生成功能

--- WMJWeiBo/WMJWeiBo/Classes/Home/WMJSQCodeViewController.swift
+++ WMJWeiBo/WMJWeiBo/Classes/Home/WMJSQCodeViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import AVFoundation
 
 class WMJSQCodeViewController: UIViewController {
 
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var codeLable: UILabel!
     @IBOutlet weak var heightConstraint: NSLayoutConstraint!
     
     @IBOutlet weak var topConstraint: NSLayoutConstraint!
@@ -22,10 +25,38 @@ class WMJSQCodeViewController: UIViewController {
         super.viewDidLoad()
         customBar.selectedItem = customBar.items?.first
         customBar.delegate = self
+        
+        startSQCode()
     }
     
     override func viewWillAppear(animated: Bool) {
         startAnimation()
+    }
+    
+    private func startSQCode(){
+        if !session.canAddInput(input){
+            return
+        }
+        
+        if !session.canAddOutput(output){
+            return
+        }
+        
+        session.addInput(input)
+        
+        session.addOutput(output)
+        
+        output.metadataObjectTypes = output.availableMetadataObjectTypes
+        
+        output.setMetadataObjectsDelegate(self, queue: dispatch_get_main_queue())
+        
+        view.layer.insertSublayer(previewLayer, atIndex:0)
+        previewLayer.frame = view.bounds
+        
+        view.layer.addSublayer(containerLayer)
+        containerLayer.frame = view.bounds
+        WMJLog(UIScreen.mainScreen().bounds.width/2)
+        session.startRunning()
     }
 
     private func startAnimation(){
@@ -43,15 +74,130 @@ class WMJSQCodeViewController: UIViewController {
     }
 
     @IBAction func intoPhotos(sender: AnyObject) {
-        WMJLog("进入相册")
+//        WMJLog("进入相册")
+        
+        if !UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary) {
+            return
+        }
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        presentViewController(imagePickerController, animated: true, completion: nil)
     }
+    
+    private lazy var input:AVCaptureDeviceInput? = {
+        let device = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        return try? AVCaptureDeviceInput(device: device)
+    }()
+    
+    private lazy var session:AVCaptureSession = AVCaptureSession()
+    
+    private lazy var output:AVCaptureMetadataOutput = {
+        let avout = AVCaptureMetadataOutput()
+        
+        let viewRect = self.view.frame
+        // 2.获取扫描容器的frame
+        let containerRect = self.containerView.frame
+        let x = containerRect.origin.y / viewRect.height;
+        let y = containerRect.origin.x / viewRect.width;
+        let width = containerRect.height / viewRect.height;
+        let height = containerRect.width / viewRect.width;
+        
+//        avout.rectOfInterest = CGRect(x: x, y: y, width: width, height: height)
+        
+        avout.rectOfInterest = CGRect(x: x, y: y, width: width, height: height)
+        
+        return avout
+    }()
+    
+    private lazy var previewLayer:AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer(session: self.session)
 
+    private lazy var containerLayer:CALayer = CALayer()
 }
+
+extension WMJSQCodeViewController:UIImagePickerControllerDelegate,UINavigationControllerDelegate{
+//    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+//        
+//    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        guard let codeImage = info[UIImagePickerControllerOriginalImage] as? UIImage else{
+            return
+        }
+        
+        let detector = CIDetector(ofType: CIDetectorTypeQRCode, context: nil, options: [CIDetectorAccuracy:CIDetectorAccuracyLow])
+        
+       let results = detector.featuresInImage(CIImage(image: codeImage)!)
+        
+        for result in results{
+            WMJLog((result as! CIQRCodeFeature).messageString)
+        }
+        picker.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
+
+extension WMJSQCodeViewController:AVCaptureMetadataOutputObjectsDelegate{
+    func captureOutput(captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [AnyObject]!, fromConnection connection: AVCaptureConnection!){
+        codeLable.text = metadataObjects.last?.stringValue
+        
+        clearLayers()
+        
+        guard let metadate = metadataObjects.last as? AVMetadataObject else{
+            return
+        }
+        
+        guard let objc = previewLayer.transformedMetadataObjectForMetadataObject(metadate) as?AVMetadataMachineReadableCodeObject else{
+            return
+        }
+   
+        drawLines(objc)
+        WMJLog(metadataObjects.last)
+    }
+    
+    private func drawLines(objc:AVMetadataMachineReadableCodeObject){
+        guard let array = objc.corners else{
+            return
+        }
+        
+        let layer = CAShapeLayer()
+        layer.lineWidth = 2
+        layer.strokeColor = UIColor.greenColor().CGColor
+        layer.fillColor = UIColor.clearColor().CGColor
+        
+        let path = UIBezierPath()
+        var point = CGPointZero
+        var index = 0
+        CGPointMakeWithDictionaryRepresentation((array[index++] as! CFDictionary), &point)
+        path.moveToPoint(point)
+        while index<array.count {
+            CGPointMakeWithDictionaryRepresentation((array[index++] as! CFDictionary), &point)
+            
+            path.addLineToPoint(point)
+        }
+        
+        path.closePath()
+        layer.path = path.CGPath
+        
+        containerLayer.addSublayer(layer)
+    }
+    
+    private func clearLayers(){
+        guard let subLayers = containerLayer.sublayers else {
+            return
+        }
+        
+        for layer in subLayers{
+            layer.removeFromSuperlayer()
+        }
+    }
+}
+
+
 
 extension WMJSQCodeViewController:UITabBarDelegate{
     func tabBar(tabBar: UITabBar, didSelectItem item: UITabBarItem) {
         
-        heightConstraint.constant = item.title == "二维码" ? 300:150
+        heightConstraint.constant = item.title == "二维码" ? 250:150
         
 //        view.layoutIfNeeded()
         
